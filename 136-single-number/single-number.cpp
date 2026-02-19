a58cd96ec1a5db365a2e6881eba12c07cca44fa6
Ojas Srivastava <108984783+ojas1888@users.noreply.github.com>
2026-02-19 20:55:57 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (82.62%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int number=0;
+        for(int i=0;i<nums.size();i++){
+            number=number^nums[i];
+        }
+        return number;
+    }
+};
