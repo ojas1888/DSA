24692206635d02221f55cd6ccbd5d231360c2a66
Ojas Srivastava <108984783+ojas1888@users.noreply.github.com>
2026-08-22 18:07:43 +0530

    Time: 3 ms (85.17%) | Memory: 128.2 MB (78.03%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -5,14 +5,14 @@ public:
         int posIndex=0;
         int negIndex=1;
         vector<int>result(n,0);
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<0){
-                result[negIndex]=nums[i];
-                negIndex=negIndex+2;
-            }
-            else{
+        for(int i=0;i<n;i++){
+            if(nums[i]>=0){
                 result[posIndex]=nums[i];
                 posIndex=posIndex+2;
+            }
+            else{
+                result[negIndex]=nums[i];
+                negIndex=negIndex+2;
             }
         }
         return result;
